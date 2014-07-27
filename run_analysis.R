1b32edb7c77a5fc07d8b7bb148e54e170c963886
LOGp <filoux@slaptop.com>
2014-07-27 15:25:05 +0200

    Just create script with steps

---
+++ run_analysis.R
@@ -0,0 +1,20 @@
+#1# Merges the training and the test sets to create one data set.
+# First, unzip the data file
+data_file = "getdata_projectfiles_UCI HAR Dataset.zip"
+unzip(data_file)
+
+
+
+#2# Extracts only the measurements on the mean and standard deviation
+#     for each measurement. 
+
+
+#3# Uses descriptive activity names to name the activities in the data set
+
+
+#4# Appropriately labels the data set with descriptive variable names. 
+
+
+#5# Creates a second, independent tidy data set with the average
+#     of each variable for each activity and each subject. 
+
